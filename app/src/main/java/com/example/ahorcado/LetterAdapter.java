59c398f40f826112690c41bb7c484db0fbb8ab72
MarioLJung <marioljung@gmail.com>
2021-10-22 17:23:50 -0300

    version1.0

---
+++ app/src/main/java/com/example/ahorcado/LetterAdapter.java
@@ -0,0 +1,50 @@
+package com.example.ahorcado;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+public class LetterAdapter extends BaseAdapter {
+    private String[] letters;
+    private LayoutInflater letterInf;
+
+    public LetterAdapter(Context context){
+        letters=new String[26];
+        for( int i=0; i< letters.length; i++){
+            letters[i]=""+(char)(i+'A');
+        }
+        letterInf=LayoutInflater.from(context);
+    }
+    @Override
+    public int getCount() {
+        return letters.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Button btnLetter;
+        if(view==null){
+            btnLetter=(Button) letterInf.inflate(R.layout.letter,viewGroup, false);
+
+        }
+        else {
+            btnLetter=(Button) view;
+        }
+        btnLetter.setText(letters[i]);
+
+        return btnLetter;
+    }
+}
